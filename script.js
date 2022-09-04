3acdd3aeb375f811262fbe062ae04f6b64daf2a5
Aristote KABELU <20kk090@esisalama.org>
2022-09-04 21:31:33 +0200

    All checking fixed

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
-document.forms[0].addEventListener("submit", function(evenement) 
-        { 
-            if (document.getElementById("name").value == "")
-            {
-                evenement.preventDefault();
-                alert("veuillez entrez le nom");
-                document.getElementById("name").focus();
-            }
-            else if (document.getElementById("first_name").value == "")
-            {
-                evenement.preventDefault();
-                alert("veuillez entrez le prenom");
-                document.getElementById("first_name").focus();
-            }
-            else if (document.getElementById("sex").value == null)
-            {
-                evenement.preventDefault();
-                alert("veuillez entrez le sexe");
-                document.getElementById("sex").focus();
-            }
-            else if (document.getElementById("birthday").value == "")
-            {
-                evenement.preventDefault();
-                alert("veuillez entrez l'année de naissance");
-                document.getElementById("year").focus();
-            }
-            else if (document.getElementById("promotion").value == "")
-            {
-                evenement.preventDefault();
-                alert("veuillez choisir la promotion");
-                document.getElementById("promotion").focus();
-            }
-            else if (document.getElementById("description").value == "")
-            {
-                evenement.preventDefault();
-                alert("veuillez entrez la description");
-                document.getElementById("description").focus();
-            }
-            
+function promotion()
+{
+    const monForm = document.forms[0];
+    const mesCheckboxes = monForm.querySelectorAll("input[type='checkbox']");
+    let emptyCase = 0;
+    for (const cb of mesCheckboxes) 
+    {  
+        if(cb.checked == false && emptyCase <= 4)
+        {
+            emptyCase++;
+        }
+    }
+    if(emptyCase == 4)
+    {
+        alert("Veuillez croché une promotion! ");
+    }
+}
 
-        });
+function sexe()
+{
+    const monForm = document.forms[0]
+    const mesCheckboxes = monForm.querySelectorAll("input[type='radio']");
+    let emptyCase = 0;
+    for (const cb of mesCheckboxes) 
+    {  
+       if(cb.checked == false && emptyCase <= 2)
+        {
+            emptyCase++;
+        }
+    }
+    if(emptyCase == 2)
+    {
+        alert("Veuillez choisir un sexe")
+    }
+}
